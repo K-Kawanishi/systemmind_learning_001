ec84e8ae8a92e57cbb78c541bdf139eb668b5090
abe05089 <k.abe@system-mind.co.jp>
2025-06-03 21:37:06 +0900

    test

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
---CREATE TABLE tasks
---(
---    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
---    summary VARCHAR(256) NOT NULL,
---    description TEXT,
---    status VARCHAR(256) NOT NULL,
---    priority VARCHAR(256) NOT NULL
---);
+CREATE TABLE tasks
+(
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    summary VARCHAR(256) NOT NULL,
+    description TEXT,
+    status VARCHAR(256) NOT NULL,
+    priority VARCHAR(256) NOT NULL
+);
