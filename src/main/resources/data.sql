0f99c22ecc303b34f148a873fadd15e57620217c
abe05089 <k.abe@system-mind.co.jp>
2025-06-03 19:50:02 +0900

    test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tasks (summary ,description ,status,priority) values ('Spring Bootを学ぶ' , 'TODOアプリを作る' , 'TODO','0');
-INSERT INTO tasks (summary ,description ,status,priority) values ('SQLを勉強する' , 'MySQLを導入する' , 'DOING','0');
-INSERT INTO tasks (summary ,description ,status,priority) values ('設計書を作る' , 'プログラム仕様書を作成してみる' , 'TODO','0');
-INSERT INTO tasks (summary ,description ,status,priority) values ('テストを実施してみる' , '試験項目書を作成する' , 'DONE','0');
+--INSERT INTO tasks (summary ,description ,status,priority) values ('Spring Bootを学ぶ' , 'TODOアプリを作る' , 'TODO','0');
+--INSERT INTO tasks (summary ,description ,status,priority) values ('SQLを勉強する' , 'MySQLを導入する' , 'DOING','0');
+--INSERT INTO tasks (summary ,description ,status,priority) values ('設計書を作る' , 'プログラム仕様書を作成してみる' , 'TODO','0');
+--INSERT INTO tasks (summary ,description ,status,priority) values ('テストを実施してみる' , '試験項目書を作成する' , 'DONE','0');
