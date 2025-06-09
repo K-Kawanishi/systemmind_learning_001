209f54b7d3a5b9ffdff95355a967ca8dae9d444b
shimadaShimada <k.shimada@system-mind.co.jp>
2025-06-09 19:52:43 +0900

    test

--- src/main/java/com/example/todo/controller/IndexController.java
+++ src/main/java/com/example/todo/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    // http://localhost:8080/ -> トップページ表示
+    // http://localhost:8080/ teststart-> トップページ表示
     @GetMapping("/")
     public String index(){
         return "index";
