7598e57e2949e5165b686486f1c551bc82b0e323
DinhSmind <t.ding@system-mind.co.jp>
2025-08-19 17:31:15 +0900

    担当者項目追加 リファクタリング　担当者制御なし

--- src/main/resources/static/ts/tasks/form.ts
+++
@@ -1,9 +0,0 @@
-//ステータス：DONE　ー＞　担当者選択できない制御
-$(document).on("change", "#statusInput", (event) => {
-    const statusElem = document.getElementById('statusInput') as HTMLInputElement | null;
-    if (statusElem?.value === "DONE") {
-        $("#pullDown").hide();
-    } else {
-        $("#pullDown").show();
-    }
-});
